2569dfec5b974ade1517b786f1f6c1596d1c9809
pa013971 <pa013971@gmail.com>
2022-11-01 21:31:38 +0800

    新增 bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap"
+
 Vue.config.productionTip = false
 
 new Vue({
