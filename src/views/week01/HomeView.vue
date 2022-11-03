f23fddf060ec6ec634f47bc260ce0b01bdd1cf7e
pa013971 <pa013971@gmail.com>
2022-11-03 13:44:14 +0800

    新增 section2 動畫及圖片

--- src/views/week01/HomeView.vue
+++ src/views/week01/HomeView.vue
@@ -57,6 +57,11 @@
 
     <div class="section2">
       <img src="@/assets/images/week01/title_sec2_outline.png" alt="title_sec2_outline" class="title_sec2_outline my-5 pt-5">
+
+      <img src="@/assets/images/week01/icon_sec2_left_gsap.png" alt="icon_sec2_left_gsap" class="icon_sec2_left_gsap">
+
+      <img src="@/assets/images/week01/icon_sec2_right_gsap.png" alt="icon_sec2_right_gsap" class="icon_sec2_right_gsap">
+
       <img src="@/assets/images/week01/block_sec2_01_flyin.png" alt="block_sec2_01_flyin" class="block_sec2_01_flyin my-4 float-end">
 
       <img src="@/assets/images/week01/block_sec2_02_flyin.png" alt="block_sec2_02_flyin" class="block_sec2_02_flyin my-4 float-start">
@@ -95,19 +100,30 @@ export default {
       .from(".text1", { rotation : -210, duration: 3 })
 
       gsap.timeline({
-      scrollTrigger: {
-        
-        trigger: ".section2",
-        start: "center center",
-        end: "bottom top",
-        markers: true,
-        scrub: true,
-        pin: true,
-      }
-    })
-    .from(".block_sec2_01_flyin", { x : -innerWidth * 1, opacity: 0 })
-    .from(".block_sec2_02_flyin", { x : innerWidth * 1, opacity: 0 })
-    .from(".block_sec2_03_flyin", { x : -innerWidth * 1, opacity: 0 })
+        scrollTrigger: {
+          trigger: ".section2",
+          start: "center center",
+          end: "bottom top",
+          markers: true,
+          scrub: true,
+          pin: true,
+        }
+      })
+      .from(".block_sec2_01_flyin", { x : -innerWidth * 1, opacity: 0 })
+      .from(".block_sec2_02_flyin", { x : innerWidth * 1, opacity: 0 })
+      .from(".block_sec2_03_flyin", { x : -innerWidth * 1, opacity: 0 })
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: ".section2",
+          markers: true,
+          scrub: true,
+          pin: true,
+        }
+      })
+      .from(".icon_sec2_left_gsap", { y : -innerWidth * 1, duration: 1 })
+      .from(".icon_sec2_right_gsap", { y : innerWidth * 1, duration: 1 }, "<")
+
     }
   }
 }
@@ -117,6 +133,7 @@ export default {
 <style scoped>
 .section1 {
   height: 100vh;
+  overflow: hidden;
 }
 
 .section1 .title {
@@ -172,23 +189,46 @@ export default {
   background: -o-linear-gradient(180deg, #1A0063 0%, #3126B0 34.67%, #FA7449 69.04%, rgba(255, 46, 0, 0) 100%);
   background: -moz-linear-gradient(180deg, #1A0063 0%, #3126B0 34.67%, #FA7449 69.04%, rgba(255, 46, 0, 0) 100%);
   background: linear-gradient(180deg, #1A0063 0%, #3126B0 34.67%, #FA7449 69.04%, rgba(255, 46, 0, 0) 100%);
+  overflow: hidden;
 }
 
-.section2 img {
+.section2 .icon_sec2_left_gsap {
+  display: flex;
+  position: absolute;
+  left: 10%;
+  bottom: 0px;
+  transform: translate(0, 50%);
+}
+
+.section2 .icon_sec2_right_gsap {
+  display: flex;
+  position: absolute;
+  right: 10%;
+  top: 0px;
+  transform: translate(0, -40%);
+}
+
+.section2 .title_sec2_outline {
   width: 100%;
   max-width: 950px;
 }
 
 .section2 .block_sec2_01_flyin {
-  transform: translate(20%, 0px);
+  transform: translate(17%, 0px);
+  width: 100%;
+  max-width: 1200px;
 }
 
 .section2 .block_sec2_02_flyin {
-  transform: translate(-20%, 0px);
+  transform: translate(-17%, 0px);
+  width: 100%;
+  max-width: 1200px;
 }
 
 .section2 .block_sec2_03_flyin {
-  transform: translate(20%, 0px);
+  transform: translate(17%, 0px);
+  width: 100%;
+  max-width: 1200px;
 }
 
 .section3 {
