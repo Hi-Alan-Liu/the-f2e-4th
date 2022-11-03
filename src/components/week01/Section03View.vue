b43503797f4e961f00900c05fb0dcdc746e84272
pa013971 <pa013971@gmail.com>
2022-11-03 16:32:05 +0800

    新增 Section03

---
+++ src/components/week01/Section03View.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="section3">
+
+    <div class="gradient_sec3_top"></div>
+
+    <img src="@/assets/images/week01/flag_sec3_top.png" alt="flag_sec3_top" class="flag_sec3_top">
+
+    <img src="@/assets/images/week01/flag_sec3_mid.png" alt="flag_sec3_mid" class="flag_sec3_mid">
+
+    <img src="@/assets/images/week01/flag_sec3_bottom.png" alt="flag_sec3_bottom" class="flag_sec3_bottom">
+
+    <div class="gradient_sec3_bottom"></div>
+
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+
+export default {
+  mounted: function() {
+    this.scrollAnimation();
+  },
+  methods: {
+    scrollAnimation() {
+
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: ".section3",
+          markers: true,
+          scrub: true,
+          pin: true,
+        }
+      })
+      .to(".flag_sec3_mid", {rotation: 12, scale: 1.2})
+      .to(".flag_sec3_top", {rotation: -6}, "<")
+      .to(".flag_sec3_bottom", {rotation: -12, scale: 1.2}, "<")
+
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.section3 {
+  height: 100vh;
+  overflow: hidden;
+}
+
+.gradient_sec3_top {
+  position: absolute;
+  width: 100%;
+  height: 200px;
+  z-index: 30;
+  background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+}
+
+.gradient_sec3_bottom {
+  position: absolute;
+  width: 100%;
+  height: 200px;
+  z-index: 30;
+
+  bottom: 0px;
+
+  background: linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 100%);
+  transform: rotate(-180deg);
+}
+
+.section3 .flag_sec3_top {
+  position: absolute;
+  width: 110%;
+  top: -30%;
+  left: -5%;
+}
+
+.section3 .flag_sec3_mid {
+  position: absolute;
+  width: 110%;
+  z-index: 20;
+  left: -5%;
+}
+
+.section3 .flag_sec3_bottom{
+  position: absolute;
+  width: 110%;
+  left: -5%;
+  top: 40%;
+}
+</style>
--- src/views/week01/HomeView.vue
+++ src/views/week01/HomeView.vue
@@ -1,11 +1,8 @@
 <template>
   <div id="app">
-
     <Section01View />
     <Section02View />
-
-    <div class="section3">
-    </div>
+    <Section03View />
   </div>
 </template>
 
@@ -17,67 +14,15 @@ gsap.registerPlugin(ScrollTrigger);
 
 import Section01View from '../../components/week01/Section01View.vue';
 import Section02View from '../../components/week01/Section02View.vue';
+import Section03View from '../../components/week01/Section03View.vue';
 
 export default {
   name: 'HomeView',
   components: {
     Section01View,
-    Section02View
-  },
-  mounted: function() {
-    this.scrollAnimation();
-  },
-  methods: {
-    scrollAnimation() {
-      gsap.timeline({
-        scrollTrigger: {
-          trigger: ".section1",
-          start: "center center",
-          end: "bottom top",
-          markers: true,
-          scrub: true,
-          pin: true,
-        }
-      })
-      .from(".dashboard-pin", { rotation : -210 })
-
-      gsap.timeline({
-        scrollTrigger: {
-          trigger: ".section2",
-          start: "center center",
-          end: "bottom top",
-          markers: true,
-          scrub: true,
-          pin: true,
-        }
-      })
-      .from(".block_sec2_01_flyin", { x : -innerWidth * 1, opacity: 0 })
-      .from(".block_sec2_02_flyin", { x : innerWidth * 1, opacity: 0 })
-      .from(".block_sec2_03_flyin", { x : -innerWidth * 1, opacity: 0 })
-
-      gsap.timeline({
-        scrollTrigger: {
-          trigger: ".section2",
-          markers: true,
-          scrub: true,
-          pin: true,
-        }
-      })
-      .from(".icon_sec2_left_gsap", { y : -innerWidth * 1, duration: 1 })
-      .from(".icon_sec2_right_gsap", { y : innerWidth * 1, duration: 1 }, "<")
-
-    }
+    Section02View,
+    Section03View
   }
 }
 
 </script>
-
-<style scoped>
-
-.section3 {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 120vh;
-}
-</style>
