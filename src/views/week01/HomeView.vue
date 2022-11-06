896def287edbe019e796df6e5a1f9ab2b9976518
pa013971 <pa013971@gmail.com>
2022-11-07 00:47:54 +0800

    新增報名機制及獎項

--- src/views/week01/HomeView.vue
+++ src/views/week01/HomeView.vue
@@ -5,6 +5,10 @@
     <SolutionView />
     <ScheduleView />
     <EventView />
+
+    <img src="@/assets/images/week01/block_sec6_yellow.png" alt="block_sec6_yellow" class="block_sec6_yellow m-5">
+    <img src="@/assets/images/week01/block_sec6_red.png" alt="block_sec6_red" class="block_sec6_red m-5">
+
   </div>
 </template>
 
